d58afbd425cbbbec30d94f444cf18821609e70c0
naukhaj <nikhil.aukhaj@prodigious.mu>
2023-07-24 14:29:38 +0400

    Revert "-first-commit"
    This reverts commit ea7ea4750eacd3779e5ee6f92d11d540ded24a49.

--- core/src/main/java/com/adobe/aem/guides/wknd/spa/angular/core/constant/ApiConstants.java
+++
@@ -1,6 +0,0 @@
-package com.adobe.aem.guides.wknd.spa.angular.core.constant;
-
-public class ApiConstants {
-    public static final String COUNTRY_URL = "http://localhost:8080/country";
-    public static final String USER_AGENT = "Mozilla/5.0";
-}
--- core/src/main/java/com/adobe/aem/guides/wknd/spa/angular/core/models/CustomComponent.java
+++
@@ -1,7 +0,0 @@
-package com.adobe.aem.guides.wknd.spa.angular.core.models;
-
-public interface CustomComponent {
-
-    public String getMessage();
-    
-}
